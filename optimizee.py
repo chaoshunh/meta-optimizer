6cdc2838298431bfc4b1841a2d78524213f503cc
Haitao Ma <mht0003@gmail.com>
2019-04-09 09:26:49 +0800

    Rename model to optimizee.

--- optimizee.py
+++ optimizee.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 class MLP(nn.Module):
     ''' A multi-layer perceptron.'''
-    def __init__(self, num_layers=20, channels=32):
+    def __init__(self, num_layers=5, channels=32):
         super(MLP, self).__init__()
                          
         self.fc0 = nn.Linear(28*28, channels) # mnist image size:28*28
@@ -15,9 +15,9 @@ class MLP(nn.Module):
 
     def forward(self, x):
         x = x.view(-1, 28*28)
-        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc0(x))
         x = torch.sigmoid(self.layers(x))
-        x = self.fc(x)
+        x = self.output(x)
         return F.log_softmax(x, dim=1)
 
     def _make_layers(self, num_layers, channels):
@@ -28,24 +28,23 @@ class MLP(nn.Module):
         return nn.Sequential(*layers)
                         
 
-class Simple_CNN(nn.Module):
+class SimpleCNN(nn.Module):
     ''' A simple convolution neural network.'''
     def __init__(self):
-        super(Simple_CNN, self).__init__()
+        super(SimpleCNN, self).__init__()
         # conv1: 1 input image channel, 6 output channels,
         #        5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(in_channels=1,
-                               out_channels=16,
-                               kernel_size=5,
-                               stride=1,
-                               padding=2)
+        self.conv1 = nn.Conv2d(1, 8, 5, padding=2)
+        self.conv2  = nn.Conv2d(8, 16, 5, padding=2)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16*14*14, 120)
-        self.fc2 = nn.Linear(120, 10)
+        self.fc1 = nn.Linear(16*7*7, 256)
+        self.fc2 = nn.Linear(256, 10)
+
 
     def forward(self, x):
         # Max pooling over a (2,2) window
         x = F.max_pool2d(F.relu(self.conv1(x)),2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -62,13 +61,13 @@ class DenseNet(nn.Module):
     pass
 
 if __name__ == "__main__":
-    mlp = MLP(num_layers=10, channels=32)
+    mlp = MLP(num_layers=2, channels=32)
     print(len(list(mlp.parameters())))
     print(len(list(mlp.children())))
 
-    scnn = Simple_CNN()
-    x = torch.randn(1, 1, 28, 28)
-    print(len(list(mlp.parameters())))
+    scnn = SimpleCNN()
+    x = torch.randn(1,1,28,28)
+    print(scnn)
     print(scnn(x))
 
         
